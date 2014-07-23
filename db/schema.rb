a1620e81b953c5dff337a196635f9262562ad7b8
WQU <wqucodebreaker@gmail.com>
2014-07-23 17:24:27 +1000

    i wanna pull too

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722100841) do
+ActiveRecord::Schema.define(version: 20140723071213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
-    t.string   "commenter"
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20140722100841) do
   end
 
   create_table "posts", force: true do |t|
-    t.string   "username"
     t.string   "title"
     t.text     "body"
     t.string   "image"
